46e9a2f8f42702b10bae9d1673c13bd34e76483a
lianlian51 <3282125330@qq.com>
2021-04-15 17:40:29 +0800

    验证二叉搜索树

---
+++ HOT100/LeetCode98/LeetCode98/LeetCode98/test.cpp
@@ -0,0 +1,28 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
+class Solution {
+public:
+	bool func(TreeNode* root, long long lower, long long upper)
+	{
+		if (root == nullptr){
+			return true;
+		}
+		if (root->val <= lower || root->val >= upper){
+			return false;
+		}
+		return func(root->left, lower, root->val) && func(root->right, root->val, upper);
+	}
+
+	bool isValidBST(TreeNode* root) {
+		return func(root, LONG_MIN, LONG_MAX);
+	}
+};
